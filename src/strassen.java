81f285107d86883623f4b8409b769c7f61e789a0
Eric Li <ericli52034@gmail.com>
2020-03-20 20:21:32 -0400

    add strassen.java

--- src/strassen.java
+++ src/strassen.java
@@ -1,136 +1,96 @@
+import java.math.BigInteger;
 import java.util.*;
+import java.io.*;
 
 public class strassen {
 
-    public static int[][] matrixAdd (int[][] m1, int[][] m2) {
-        int[][] result = new int[m1.length][m1[0].length];
-
-        for (int i = 0; i < m1.length; i++) {
-            for (int j = 0; j < m1[0].length; j++) {
-                result[i][j] = m1[i][j] + m2[i][j];
-            }
-        }
-
-        return result;
-    }
-
-    public static int[][] matrixScale (int[][] m, int k) {
-        int[][] result = new int[m.length][m[0].length];
-
-        for (int i = 0; i < m.length; i++) {
-            for (int j = 0; j < m[0].length; j++) {
-                result[i][j] = k * m[i][j];
-            }
-        }
-
-        return result;
-    }
-
-    public static int[][] matrixMult (int[][] m1, int[][] m2) {
-        int m1Rows = m1.length;
-        int m1Cols = m1[0].length;
-        int m2Cols = m2[0].length;
-
-        int[][] result = new int[m1Rows][m2Cols];
-
-        for (int m1Row = 0; m1Row < m1Rows; m1Row++) {
-            for (int m2Col = 0; m2Col < m2Cols; m2Col++) {
-                int currentElement = 0;
-
-                for (int k = 0; k < m1Cols; k++) {
-                    currentElement += m1[m1Row][k] * m2[k][m2Col];
-                }
-
-                result[m1Row][m2Col] = currentElement;
-            }
-        }
-
-        return result;
-    }
-
-    public static int[][] subMatrix (int[][] m, int xStart, int xEnd, int yStart, int yEnd) {
-        int[][] res = new int[xEnd-xStart+1][yEnd-yStart+1];
-        for (int j = yStart; j <= yEnd; j++) {
-            res[j-yStart] = Arrays.copyOfRange(m[j], xStart, xEnd+1);
-        }
-        return res;
-    }
-
-    public static int[][] combineMatrices (int[][] topleft, int[][] topright, int[][] botleft, int[][] botright) {
-        int dim = topleft.length;
-        int newDim = 2 * dim;
-        int[][] res = new int[newDim][newDim];
-        for (int i = 0; i < dim; i++) {
-            for (int j = 0; j < dim; j++) {
-                res[i][j] = topleft[i][j];
-                res[i][j+dim] = topright[i][j];
-                res[i+dim][j] = botleft[i][j];
-                res[i+dim][j+dim] = botright[i][j];
-            }
-        }
-        return res;
-    }
-
-    public static int[][] strassenMult (int[][] m1, int[][] m2) {
-        if (m1.length <= 1) {
-            return matrixMult(m1, m2);
-        }
-
-        int n = m1.length;
-
-        int[][] A = subMatrix(m1, 0, (n-1)/2, 0, (n-1)/2);
-        int[][] B = subMatrix(m1, (n+1)/2, n-1, 0, (n-1)/2);
-        int[][] C = subMatrix(m1, 0, (n-1)/2, (n+1)/2, n-1);
-        int[][] D = subMatrix(m1, (n+1)/2, n-1, (n+1)/2, n-1);
-
-        int[][] E = subMatrix(m2, 0, (n-1)/2, 0, (n-1)/2);
-        int[][] F = subMatrix(m2, (n+1)/2, n-1, 0, (n-1)/2);
-        int[][] G = subMatrix(m2, 0, (n-1)/2, (n+1)/2, n-1);
-        int[][] H = subMatrix(m2, (n+1)/2, n-1, (n+1)/2, n-1);
-
-        int[][] P1 = strassenMult(A, matrixAdd(F, matrixScale(H, -1)));
-        int[][] P2 = strassenMult(matrixAdd(A, B), H);
-        int[][] P3 = strassenMult(matrixAdd(C, D), E);
-        int[][] P4 = strassenMult(D, matrixAdd(G, matrixScale(E, -1)));
-        int[][] P5 = strassenMult(matrixAdd(A, D), matrixAdd(E, H));
-        int[][] P6 = strassenMult(matrixAdd(B, matrixScale(D, -1)), matrixAdd(G, H));
-        int[][] P7 = strassenMult(matrixAdd(A, matrixScale(C, -1)), matrixAdd(E, F));
-
-        int[][] topleft = matrixAdd(P5, matrixAdd(P4, matrixAdd(matrixScale(P2, -1), P6)));
-        int[][] topright = matrixAdd(P1, P2);
-        int[][] botleft = matrixAdd(P3, P4);
-        int[][] botright = matrixAdd(P5, matrixAdd(P1, matrixScale(matrixAdd(P3, P7), -1)));
-
-        return combineMatrices(topleft, topright, botleft, botright);
-    }
-
-    public static int[][] generateMatrix(int dim) {
-        Random rnd = new Random();
-        int[][] m = new int[dim][dim];
-        for (int i = 0; i < dim; i++) {
-            for (int j = 0; j < dim; j++) {
-                m[i][j] = rnd.nextInt(2);
-            }
-        }
-        return m;
-    }
-
-    public static void main(String[] args) {
+    public static void tests() {
         int[][] m = {{1,1,1,1},{2,0,2,4},{0,1,0,3},{1,0,0,1}};
-        int[][] randm = generateMatrix(16);
+        int[][] randm = MatrixOps.generateMatrix(256);
 
         //int[][] prod = matrixMult(m, m);
         //int[][] prod2 = strassenMult(m, m);
+        long startTime = System.nanoTime();
+        int[][] randprod = MatrixOps.matrixMult(randm, randm);
+        long endTime = System.nanoTime();
+        System.out.println("Regular: " + (double)(endTime-startTime)/1000000000);
 
-        int[][] randprod = matrixMult(randm, randm);
-        int[][] randprod2 = strassenMult(randm, randm);
+        long start2 = System.nanoTime();
+        int[][] randprod2 = MatrixOps.strassenMult(randm, randm);
+        long end2 = System.nanoTime();
+        System.out.println("Strassen: " + (double)(end2-start2)/1000000000);
+
+        // System.out.println(Arrays.deepEquals(randprod, randprod2));
 
         //System.out.println(Arrays.deepToString(prod));
         //System.out.println(Arrays.deepToString(prod2));
 
-        System.out.println(Arrays.deepToString(randm));
-        System.out.println(Arrays.deepToString(randprod));
-        System.out.println(Arrays.deepToString(randprod2));
+        //System.out.println(Arrays.deepToString(randm));
+        //System.out.println(Arrays.deepToString(randprod));
+        //System.out.println(Arrays.deepToString(randprod2));
+
+        //int[][] r = {{1,1,1},{2,0,2},{0,1,0}};
+        //System.out.println(Arrays.deepToString(pad(3, r)));
+
+        /*int[][] k = {{1,1,1,1,1},{2,0,2,4,1},{0,1,0,3,1},{1,0,0,1,1}, {2,3,1,4,3}};
+        int[][] prod3 = strassenMult(k, k);
+        System.out.println(Arrays.deepToString(prod3));*/
+    }
+
+    public static void graphTest() {
+        double p = 0.01;
+
+        RandomGraph g = new RandomGraph(1024, p);
+        int[][] A = g.getAdjacency();
+        int[][] A3 = MatrixOps.strassenMult(A, MatrixOps.strassenMult(A, A));
+        int sumDiag = 0;
+        for (int i = 0; i < A3.length; i++) {
+            sumDiag += A3[i][i];
+        }
+        System.out.println("Number of triangles: "+(sumDiag/6));
+
+        System.out.println("Expected number of triangles: "+((binom(1024,3).longValue())*Math.pow(p, 3)));
+    }
+
+    public static BigInteger binom(final int N, final int K) {
+        ArrayList<BigInteger> arr = new ArrayList<BigInteger>();
+        arr.add(BigInteger.ONE);
+        for (int k = 1; k <= K; k++) {
+            arr.add(arr.get(k-1).multiply(BigInteger.valueOf(N-k+1)).divide(BigInteger.valueOf(k)));
+        }
+        return arr.get(K);
+    }
+
+    public static void main(String[] args) throws FileNotFoundException {
+        int dim = Integer.parseInt(args[1]);
+        String filename = args[2];
+
+        File file = new File("./"+filename);
+        Scanner fileScanner = new Scanner(file);
+
+        int[][] m = new int[dim][dim];
+        int[][] m2 = new int[dim][dim];
+
+        for (int i = 0; i < dim; i++) {
+            for (int j = 0; j < dim; j++) {
+                m[i][j] = fileScanner.nextInt();
+            }
+        }
+
+        for (int i = 0; i < dim; i++) {
+            for (int j = 0; j < dim; j++) {
+                m2[i][j] = fileScanner.nextInt();
+            }
+        }
+
+        int[][] prod = MatrixOps.strassenMult(m, m2);
+
+        for (int i = 0; i < dim; i++) {
+            System.out.println(prod[i][i]);
+        }
+
+        // graphTest();
+
     }
 
 }
